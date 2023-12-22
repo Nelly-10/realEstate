6737e9cc7a91b7a96f6182d3f4e9956ee38a557b
Nelly-10 <nelson_nonso@yahoo.com>
2023-12-22 19:26:41 +0100

    first commit

---
+++ src/component/GetStarted/GetStarted.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './GetStarted.css';
+
+const GetStarted = () => {
+  return (
+        <section className='g-wrapper'>
+            <div className='paddings innerWidth g-container'>
+                <div className='flexColCenter inner-container'>
+                    <span className='primaryText'>Get started with Homzy</span>
+                    <span className='secondaryText'>Subscribe and find super attractive price quote from <br />
+                    Find your residence soon</span>
+                    <button className='button'>
+                        <a href='"mailto:odumahnelson300@gmail.com'>Get Started</a>
+                    </button>
+                </div>
+            </div>
+        </section>
+  )
+}
+
+export default GetStarted;
